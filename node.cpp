4c2e81f53b0a6a7ea939cc2cb0da283f3964cf8b
Tinsed <black-corven@mail.ru>
2016-09-18 02:55:24 +0300

    Update

--- node.cpp
+++ node.cpp
@@ -1,15 +1,15 @@
 #include <node.h>
 
 Node::Node(){
-	char s[3][3] = {{'7', '4', '2'}, {'3','4','8'}, {'6', 'x', '1'}};
-	state = State((char**)s,(int)0x21);
+	char s[3][3] = {{'x', '2', '3'}, {'1','7','5'}, {'4', '6', '8'}};
+	state = new State(s,0,0);
 	pParentNode = nullptr;
-	iAction = NUF;
+	iAction = -1;
 	iPathCost = 0;
 	iDepth =0;
 }
 
-Node::Node(State s, Node* parrent, int act, int cost, int depth){
+Node::Node(State* s, Node* parrent, int act, int cost, int depth){
 	state = s;
 	pParentNode = parrent;
 	iAction = act;
